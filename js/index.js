ca1a5c444d582408dc6fad80f33b72e8b6dfae04
Darien82 <DarienCurry@gmail.com>
2016-03-09 23:45:20 -0800

    no winner

--- js/index.js
+++ js/index.js
@@ -27,18 +27,18 @@ $(document).ready(function(){
     var space7 = $("#board tr:nth-child(3) td:nth-child(1)").text();
     var space8 = $("#board tr:nth-child(3) td:nth-child(2)").text();
     var space9 = $("#board tr:nth-child(3) td:nth-child(3)").text();
-    // check rows
+    
     if      ((space1==space2) && (space2==space3)) { return space3; }
     else if ((space4==space5) && (space5==space6)) { return space6; }	
     else if ((space7==space8) && (space8==space9)) { return space9; }
-    // check columns
+   
     else if ((space1==space4) && (space4==space7)) { return space7; }
     else if ((space2==space5) && (space5==space8)) { return space8; }
     else if ((space3==space6) && (space6==space9)) { return space9; }
-    // check diagonals
+   
     else if ((space1==space5) && (space5==space9)) { return space9; }
     else if ((space3==space5) && (space5==space7)) { return space7; }
-    // no winner
+    
     return -1;
   }
 
